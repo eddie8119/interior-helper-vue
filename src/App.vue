e78129764492027cf2c34c4821c01be5304e201e
eddie wang <wang8119@gmail.com>
2025-09-24 23:10:48 +0800

    isQuickDraftPage

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <RouterView />
-  <QuickDraftSlide />
-  <QuickDraftToggle />
+  <QuickDraftSlide v-if="!isQuickDraftPage" />
+  <QuickDraftToggle v-if="!isQuickDraftPage" />
 </template>
 
 <script setup lang="ts">
-import { provide, ref } from 'vue';
+import { provide, ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
 
 import QuickDraftSlide from '@/components/core/draft/QuickDraftSlide.vue';
 import QuickDraftToggle from '@/components/core/draft/QuickDraftToggle.vue';
 import { useAuthStore } from '@/stores/auth';
 import { initTheme, setTheme } from '@/utils/theme';
 
+const route = useRoute();
 const authStore = useAuthStore();
 authStore.initializeAuthState();
+
+const isQuickDraftPage = computed(() => route.path === '/todo/quick-draft');
 
 // 主題顏色
 const isDarkMode = ref<boolean>(initTheme());

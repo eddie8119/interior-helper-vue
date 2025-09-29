fa2c1c8752dfe3ed49521ee9d56620a624776b7a
eddie wang <wang8119@gmail.com>
2025-09-29 17:29:56 +0800

    remove file

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,7 @@ import express from 'express';
 
 import authRoutes from './auth';
 import commonRoutes from './common';
-import companyRoutes from './company';
-import contractRoutes from './contract';
 import draftRoutes from './draft';
-import invoiceRoutes from './invoice';
 import projectRoutes from './project';
 import taskRoutes from './task';
 import userRoutes from './user';
@@ -17,9 +14,6 @@ app.use('/api/user', userRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/common', commonRoutes);
-app.use('/api', invoiceRoutes);
-app.use('/api', companyRoutes);
-app.use('/api', contractRoutes);
 app.use('/api/draft', draftRoutes);
 
 export default app;

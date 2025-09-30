724e96717d67002af4d1d54852421d52866d0436
LAPTOP-CAH83BQA\eddie <wang8119@gmail.com>
2025-09-30 17:38:15 +0800

    add ProgressBar

--- src/components/core/situationRoom/ProjectsBulletinBoard.vue
+++ src/components/core/situationRoom/ProjectsBulletinBoard.vue
@@ -16,6 +16,9 @@
       <template #type="{ row }">
         <p>{{ t(`project.type.${row.type}`) }}</p>
       </template>
+      <template #ph_measurement_value="{ row }">
+        <!-- <ProgressBar :value="row.ph ?? 0" :observation-type="props.observationType" /> -->
+      </template>
     </Table>
   </div>
 </template>
@@ -24,7 +27,7 @@
 import { computed, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-import StatusShow from '@/components/core/StatusShow.vue';
+import ProgressBar from '@/components/core/chart/ProgressBar.vue';
 import Table from '@/components/core/table/Table.vue';
 import { useProjects } from '@/composables/useProjects';
 import { PROJECT_COLUMNS } from '@/constants/columns/project';

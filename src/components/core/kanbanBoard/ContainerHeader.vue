c595beec6a2e1fb3f1d8d276351e1a06f9d45fd7
LAPTOP-CAH83BQA\eddie <wang8119@gmail.com>
2025-10-02 12:23:16 +0800

    add TrashButton

--- src/components/core/kanbanBoard/ContainerHeader.vue
+++ src/components/core/kanbanBoard/ContainerHeader.vue
@@ -7,26 +7,7 @@
         @update:construction-name="updateConstructionName"
       />
     </div>
-    <button
-      class="rounded-full p-1 text-red-500 hover:bg-red-100"
-      title="刪除工程類型"
-      @click="showDeleteConstructionDialog = true"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-        />
-      </svg>
-    </button>
+    <TrashButton @click="showDeleteConstructionDialog = true" />
 
     <!-- 刪除確認對話框 -->
     <DeleteDialog
@@ -44,6 +25,7 @@ import { useI18n } from 'vue-i18n';
 import DeleteDialog from '@/components/core/dialog/DeleteDialog.vue';
 import ContainerTitle from '@/components/core/kanbanBoard/ContainerTitle.vue';
 import DragHandle from '@/components/ui/DragHandle.vue';
+import TrashButton from '@/components/ui/TrashButton.vue';
 
 defineProps<{
   constructionName: string;
--- src/components/core/kanbanBoard/ContainerItem.vue
+++ src/components/core/kanbanBoard/ContainerItem.vue
@@ -23,21 +23,7 @@
         class="flex items-center justify-center rounded-md bg-blue-100 px-3 py-1 text-blue-700 hover:bg-blue-200"
         @click="startEditing"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="mr-1 h-4 w-4"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
-        施作項目
+        + 施作項目
       </button>
     </div>
   </div>

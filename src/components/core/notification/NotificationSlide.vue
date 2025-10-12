01453c5900befc75e2a737d99dec4c63f80fe9f3
eddie wang <wang8119@gmail.com>
2025-10-13 01:21:46 +0800

    slide ui

---
+++ src/components/core/notification/NotificationSlide.vue
@@ -0,0 +1,25 @@
+<template>
+  <SlidePanel
+    v-model="isNotificationSlideVisible"
+    side="right"
+    :topOffsetRem="4"
+    :widthPercent="45"
+    :maxWidthPx="350"
+    @close="hideNotificationSlide"
+  >
+    <!-- Notification slot content goes here -->
+    <div class="min-h-full">
+      <!-- Notification -->
+    </div>
+  </SlidePanel>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import SlidePanel from '@/components/ui/SlidePanel.vue';
+import { useSlideStore } from '@/stores/slide';
+
+const slideStore = useSlideStore();
+const { isNotificationSlideVisible } = storeToRefs(slideStore);
+const { hideNotificationSlide } = slideStore;
+</script>

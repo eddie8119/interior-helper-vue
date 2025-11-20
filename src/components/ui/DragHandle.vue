bb69a9b3626023203a2a8678adaa638f9b8c6f62
LAPTOP-CAH83BQA\eddie <wang8119@gmail.com>
2025-11-20 09:35:02 +0800

    fix: DragHandle 組件缺少了 drag-handle

--- src/components/ui/DragHandle.vue
+++ src/components/ui/DragHandle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="[handleClass, 'cursor-grab rounded-full p-1 hover:bg-gray-200']">
+  <div :class="[handleClass, 'drag-handle cursor-grab rounded-full p-1 hover:bg-gray-200']">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       :class="iconClass"

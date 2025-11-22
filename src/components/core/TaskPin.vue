327f3dbe249f56f0ca3b701f7b3d9f3bb6fd913a
LAPTOP-CAH83BQA\eddie <wang8119@gmail.com>
2025-11-22 12:11:40 +0800

    add: TaskPin

---
+++ src/components/core/TaskPin.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col items-center text-center">
+    <div
+      class="ring-black/5 mt-1 max-w-[160px] rounded bg-white/95 px-2 py-1 text-xs font-medium leading-tight text-gray-700 shadow-md ring-1"
+    >
+      {{ title }}
+    </div>
+    <img src="@/assets/icons/PinGreen.png" alt="Pin" class="mt-1 h-6 w-6" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+}>();
+</script>
